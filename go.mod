5d9cd0727e7b5efab364f2bbb80d38224774c16a
Nikolay Sedov <nicksedov@yandex.ru>
2026-02-01 16:13:53 +0300

    Read env vars from file

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.23
 require (
 	github.com/disintegration/imaging v1.6.2
 	github.com/gin-gonic/gin v1.9.1
+	github.com/joho/godotenv v1.5.1
 	gorm.io/driver/postgres v1.5.4
 	gorm.io/gorm v1.25.5
 )
--- main.go
+++ main.go
@@ -6,7 +6,16 @@ import (
 	"log"
 	"os"
 	"strings"
+	"github.com/joho/godotenv"
 )
+
+// init is invoked before main()
+func init() {
+    // loads values from .env into the system
+    if err := godotenv.Load(); err != nil {
+        log.Print("No .env file found")
+    }
+}
 
 func main() {
 	// Parse command line arguments
